d8d87f064d341b4218684a6343340f36f2fb0eec
hcf <iloveyout-mac@qq.com>
2020-04-01 18:34:15 +0800

    添加函数防抖总结，以及公用函数的添加

--- common-hcf/hcf-js.js
+++ common-hcf/hcf-js.js
@@ -5,4 +5,30 @@
  * @warning   
  * 
  */
-//在设置字体或者文字居中的时候使用到的vertical-align:middle;是相对于子元素设置，对父元素无效果。。。。
+
+/**
+* @desc 函数防抖（debounce）
+* @param func 目标函数
+* @param waitTime 延迟执行毫秒数
+* @param immediate true - 立即执行， false - 延迟执行
+*/
+function deboundce(func, waitTime, immediate) {
+    let timer;
+    return function () {
+        let content = this;
+        let args = arguments;
+
+        if (timer) clearTimeout(timer);
+        if (immediate) {
+            let callNow = !timer;
+            timer = setTimeout(() => {
+                timer = null;
+            }, waitTime);
+            if (callNow) func.apply(content, args);
+        } else {
+            timer = setTimeout(() => {
+                func.apply(this, args)
+            }, waitTime)
+        }
+    }
+}
